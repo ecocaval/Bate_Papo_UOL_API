7e970d6a71d5610a8a508ef41cff46486ac73b46
erico <ecocaval@hotmail.com>
2023-01-13 16:17:45 -0300

    adding feature: "Sanitização de dados"

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { MongoClient, ObjectId } from "mongodb"
 import dayjs from 'dayjs'
 import { participantSchema, messageSchema } from '../schemas/schemas.js'
+import { stripHtml } from "string-strip-html"
 
 const PORT = 5000
 const app = express()
@@ -29,7 +30,9 @@ app.post("/participants", async (req, res) => {
 
     try {
 
-        const participant = await participantSchema.validateAsync(req.body)
+        let participant = await participantSchema.validateAsync(req.body)
+
+        participant = sanitizeAndTrim(participant)
 
         const usernameInUse = await db.collection("participants").findOne(participant)
 
@@ -37,13 +40,17 @@ app.post("/participants", async (req, res) => {
 
         await db.collection("participants").insertOne({ ...participant, lastStatus: Date.now() })
 
-        await db.collection("messages").insertOne({
+        let messageToInsert = {
             from: participant.name,
             to: 'Todos',
             text: 'entra na sala...',
             type: 'status',
             time: dayjs(Date.now()).format('HH:mm:ss')
-        })
+        }
+
+        messageToInsert = sanitizeAndTrim(messageToInsert)
+
+        await db.collection("messages").insertOne(messageToInsert)
 
         return res.sendStatus(201)
 
@@ -81,11 +88,15 @@ app.post("/messages", async (req, res) => {
 
         if (!userExists) return res.sendStatus(422)
 
-        const messageWasPosted = await db.collection("messages").insertOne({
+        let messageToPost = {
             from: user,
             ...message,
             time: dayjs(Date.now()).format('HH:mm:ss')
-        })
+        }
+
+        messageToPost = sanitizeAndTrim(messageToPost)
+
+        const messageWasPosted = await db.collection("messages").insertOne(messageToPost)
 
         if (messageWasPosted) return res.sendStatus(201)
 
@@ -103,16 +114,16 @@ app.get("/messages", async (req, res) => {
     try {
         const { query } = req
         const { user } = req.headers
-        
+
         const allMessages = await db.collection("messages").find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] }).toArray()
-        
+
         if (query.limit) {
             const messagesLimit = Number(query.limit)
 
             if (messagesLimit < 1 || isNaN(messagesLimit)) return res.sendStatus(422)
-            
+
             return res.send([...allMessages].slice(-messagesLimit).reverse())
-        }        
+        }
 
         return res.send([...allMessages].reverse())
 
@@ -200,4 +211,13 @@ function checkInactiveUsers() {
         }
 
     }, timeTolerance)
+}
+
+function sanitizeAndTrim(obj) {
+
+    for (const [key, value] of Object.entries(obj)) {
+        obj[key] = stripHtml(value).result.trim();
+    }
+
+    return obj;
 }
